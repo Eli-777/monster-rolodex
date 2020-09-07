fc6cfc111476c75cc87ce2858fa5b2927b037588
Eli <jo45840@hotmail.com>
2020-09-07 18:36:53 +0800

    Updates

---
+++ precache-manifest.b97d45d1368d95a679305500aec56aa0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14959ea64b473388b4794b9423ed925d",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "bcd2e3a72c14a682b6d2",
+    "url": "/monster-rolodex/static/css/main.d2fa65e3.chunk.css"
+  },
+  {
+    "revision": "10ffa0be4341c6a9750a",
+    "url": "/monster-rolodex/static/js/2.8facccc6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.8facccc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcd2e3a72c14a682b6d2",
+    "url": "/monster-rolodex/static/js/main.617e887d.chunk.js"
+  },
+  {
+    "revision": "9683b9f73c707a9cca82",
+    "url": "/monster-rolodex/static/js/runtime-main.89bf87a9.js"
+  }
+]);
